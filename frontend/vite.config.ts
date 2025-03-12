4b0da6c2672a148501fd79bb625c9f62e9818bfb
Sofiane <s.msatfa@ecole-ipssi.net>
2025-03-12 12:43:49 +0100

    ajout(ops): ajout docker dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,20 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 4173,
+    host: true,
+  },
+  server: {
+    port: 5173,
+    host: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
